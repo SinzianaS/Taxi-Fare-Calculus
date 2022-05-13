15eb181ecd9563f398d6e649edee370a40dc476a
Sinziana Sovejanu <sv.sinziana@gmail.com>
2022-05-13 23:54:33 +0300

    this is the first commit

---
+++ TaxiFareCalculus/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace TaxiFareCalculus
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int timeOfDay = Convert.ToInt32(Console.ReadLine());
+            int distKm = Convert.ToInt32(Console.ReadLine());
+            int totalPay = 0;
+            if (timeOfDay >= 8 && timeOfDay < 21)
+            {
+                if (distKm < 20)
+                {
+                    totalPay = distKm * 5;
+                }
+
+                else if (distKm >= 21 && distKm <= 60)
+                {
+                    totalPay = distKm * 4 * 2;
+                }
+
+                else if (distKm > 60)
+                {
+                    totalPay = distKm * 3 * 2;
+                }
+            }
+
+            else
+            { 
+                if (distKm <= 20)
+                {
+                    totalPay = distKm * 7;
+                }
+
+                else if (distKm > 20 && distKm <= 60)
+                {
+                    totalPay = distKm * 5 * 2;
+                }
+
+                else 
+                {
+                    totalPay = distKm * 4 * 2;
+                }
+            }
+            
+
+            Console.WriteLine(totalPay);
+            Console.ReadLine();
+        }
+    }
+}
+
